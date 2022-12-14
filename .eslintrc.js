31466c78e8def3f2da3eeccda409d61ba747b90b
tanthuann <truongtanthuan99@gmail.com>
2022-12-14 22:00:41 +0700

    feat: config linter

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react", "jest"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "linebreak-style": 0,
+    quotes: 0,
+    "comma-dangle": 0
+  }
+};
