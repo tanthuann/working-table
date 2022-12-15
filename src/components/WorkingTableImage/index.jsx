94388432f0475c1859ef3cecb5f37650c5aa634e
tanthuann <truongtanthuan99@gmail.com>
2022-12-15 20:48:07 +0700

    chore: rename component

---
+++ src/components/WorkingTableImage/index.jsx
@@ -0,0 +1,10 @@
+// others
+import "./styles.scss";
+
+const WorkingTableImage = () => (
+  <div className="working-table-image-wrapper">
+    <img src="working-table.svg" alt="working-table" />
+  </div>
+);
+
+export default WorkingTableImage;
